addb86ac2adb41fe3408ac11c46780a6c70226f6
Victoria Igbojekwe <toriaokechukwu@gmail.com>
2023-02-16 16:50:29 -0800

    Writing a c program using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * prints a string and returns 0 then exits
+ * Return: int(0) 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
