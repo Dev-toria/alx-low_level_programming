be25ad724558562864d3fc0a9cfdfe72243f024e
Victoria Igbojekwe <toriaokechukwu@gmail.com>
2023-02-16 17:18:45 -0800

    Printing a string with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *
+ * print a string and then return 0 then exits
+ * Return: int(0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
