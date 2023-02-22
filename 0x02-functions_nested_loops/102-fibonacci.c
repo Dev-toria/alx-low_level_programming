fa259a77ec797b5e3d7ea532f8fc97297a0cda3c
Victoria Igbojekwe <toriaokechukwu@gmail.com>
2023-02-22 14:22:12 -0800

    fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+	long t1 = 1;
+	long t2 = 2;
+	long next_term = t1 + t2;
+
+	printf("%ld, %ld, ", t1, t2);
+	for (i = 3; i <= 50; i++)
+	{
+		printf("%ld, ", next_term);
+		t1 = t2;
+		t2 = next_term;
+		next_term = t1 + t2;
+	}
+	printf("\n");
+	return (0);
+}
