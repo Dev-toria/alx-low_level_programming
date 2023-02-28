f289885711d46a1105adb4ba5c8de56f5b3587cb
Victoria Igbojekwe <toriaokechukwu@gmail.com>
2023-02-27 18:07:24 -0800

    Task 11

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
