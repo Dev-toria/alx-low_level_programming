bc6acb1f086c6b0365ec932c011b2252a11fe83e
victoria igbojekwe <toriaokechukwu.com>
2023-03-30 15:31:25 -0700

    fix a little error

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,6 +10,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int length = 0;
 
 	new_node = malloc(sizeof(list_t));
 
@@ -17,8 +18,11 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	while (str[length])
+		length++;
+
+	new_node->len = length;
 	new_node->str = strdup(str);
-	new_node->next = NULL;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
