3b2c31cc95b0d0fa25b0e94aa4bc087830aec3e0
victoria igbojekwe <toriaokechukwu.com>
2023-04-11 13:39:59 -0700

    added 6-pop_listint.c file

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - Deletes the head node of a listint_t linked list.
+ * @head: Double pointer to the head node of the linked list
+ *
+ * Return: The head node's data (n), or 0 if the list is empty.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	n = temp->n;
+	*head = (*head)->next;
+	free(temp);
+	return (n);
+}
